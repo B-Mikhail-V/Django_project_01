1cef18346df642da38f5e238709c8a7e4bf1ed92
B-Mikhail-V <bakulin.mv@gmail.com>
2022-04-17 21:23:33 +0300

    17_04

---
+++ 1.2-requests-templates/1111.py
@@ -0,0 +1,7 @@
+x = [1,2]
+y = x.append(3)
+print(x.append(3))
+
+arr = (1, 2, 3)
+# string = str(arr)
+print(arr[0])
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -3,4 +3,12 @@ from django.db import models
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    id = models.CharField(primary_key=True, max_length=100)
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    image = models.FilePathField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=100, db_index=True, verbose_name='URL')
+
+
